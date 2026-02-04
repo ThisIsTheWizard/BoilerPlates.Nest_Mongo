5babaf93fee5723220270fa25b1ae88ebc043135
Elias Shekh <55489470+ThisIsTheWizard@users.noreply.github.com>
2026-02-04 06:34:40 +0600

    chore: Remove Prisma configuration file containing seed logic.

--- src/prisma/prisma.config.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from '@prisma/client'
-
-const prisma = new PrismaClient()
-
-export default {
-  seed: async () => {
-    await prisma.$executeRaw`ts-node src/prisma/seed.ts`
-  }
-}
